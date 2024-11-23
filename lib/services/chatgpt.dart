2b55c78094c906717efe592db6ed5475898e4aae
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-11-24 01:56:18 +0700

    feat: Update chatgpt url

--- lib/services/chatgpt.dart
+++ lib/services/chatgpt.dart
@@ -1,1 +1,22 @@
-// import 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Chatgpt {
+// POST: https://nihongobenkyou.online/api/chat/
+// {
+//     "message": "Chào bạn Nhàn"
+// }
+  Future<String> getResponse(String message) async {
+    final response = await http.post(
+      Uri.parse('https://nihongobenkyou.online/api/chat/'),
+      headers: {"Content-Type": "application/json"},
+      body: '{"message": "$message"}',
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "Error processing message.";
+    }
+  }
+}
